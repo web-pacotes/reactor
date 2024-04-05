ff0bbc88340bda8def3ee287e7f710d05c76f92b
freitzzz <1160907@isep.ipp.pt>
2024-04-05 09:43:59 +0100

    feat(reactor-svelte): add implementation of a svelte store that allows emiting duplicate values

---
+++ packages/reactor-svelte/src/lib/store.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test, vitest } from 'vitest';
+import { allowDuplicatesStore } from './store.js';
+
+describe('allowDuplicatesStore', () => {
+	test('emits value if the previous is a different value', () => {
+		const store = allowDuplicatesStore(0);
+		const mockSubscribe = vitest.fn<number[], void>()
+		const value = 1;
+
+		store.subscribe(mockSubscribe);
+		store.set(value);
+
+		expect(mockSubscribe).toBeCalledWith(value)
+	});
+
+	test('emits value if the previous is the same value', () => {
+		const store = allowDuplicatesStore(0);
+		const mockSubscribe = vitest.fn<number[], void>()
+		const value = 1;
+
+		store.subscribe(mockSubscribe);
+		store.set(value);
+		store.set(value);
+
+		expect(mockSubscribe).toHaveBeenLastCalledWith(value);
+		expect(mockSubscribe).toBeCalledTimes(3);
+	});
+});
---
+++ packages/reactor-svelte/src/lib/store.ts
@@ -0,0 +1,28 @@
+import { writable } from "svelte/store";
+
+/**
+ * Creates a new svelte {@link writable} store that allows emiting duplicate values.
+ * 
+ * @returns a {@link writable} store that doesn't check if the previous element is the same as the emiting one.
+ */
+export function allowDuplicatesStore<T>(initial: T | undefined = undefined) {
+    const { update, subscribe } = writable<{ value: T | undefined, inc: number }>(
+        { value: initial, inc: 0 }
+    );
+
+    return {
+        set: (value: T) => update((v) => {
+            return {
+                value: value,
+                inc: v.inc + 1
+            };
+        }),
+        update: (updater: (value: T) => T) => update((v) => {
+            return {
+                value: updater(v.value!),
+                inc: v.inc + 1
+            };
+        }),
+        subscribe: (callback: (value: T) => void) => subscribe((event) => callback(event.value!))
+    };
+}
