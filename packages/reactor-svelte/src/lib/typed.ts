3015737eb9ad7bc93f230f9291bf405a2764b5ea
freitzzz <1160907@isep.ipp.pt>
2024-04-10 21:01:12 +0100

    feat(reactor-svelte): define type that marks class definitions

--- packages/reactor-svelte/src/lib/typed.ts
+++ packages/reactor-svelte/src/lib/typed.ts
@@ -5,6 +5,12 @@
 type TypedObject = { type: string };
 
 /**
+ * A type that is useful to identify class definitions. The library uses this type to allow clients
+ * to pass the class of reactors they want to resolve.
+ */
+export type TypedClass<T> = new (...args: never[]) => T;
+
+/**
  * Type guard for objects that represent their types via the `type` property.
  *
  * @param value - the object being type guarded
