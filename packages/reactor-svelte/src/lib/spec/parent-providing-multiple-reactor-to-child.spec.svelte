7ba0f3569e172dc59ea4fa9ebe3ad8f9fd1eb848
freitzzz <1160907@isep.ipp.pt>
2024-04-17 22:18:25 +0100

    test(reactor-svelte): add component tests for multi reactor provider

---
+++ packages/reactor-svelte/src/lib/spec/parent-providing-multiple-reactor-to-child.spec.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { MultiReactorProvider, resolve } from '../index.js';
+	import { CounterReactor } from './counter-reactor.js';
+	import { NumberReactor } from './number-reactor.js';
+	import AttachDomValueChild from './attach-dom-value.spec.svelte';
+
+	export let key: string;
+
+	const counter = new CounterReactor();
+	const number = new NumberReactor();
+
+	const reactors = [counter, number];
+</script>
+
+<MultiReactorProvider {reactors}>
+	<AttachDomValueChild
+		{key}
+		callback={() => `${resolve(CounterReactor)}|${resolve(NumberReactor)}`}
+	/>
+</MultiReactorProvider>
