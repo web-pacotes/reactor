cdeb199440efc8c2b9b4dadabcb98ba2c917123d
freitzzz <1160907@isep.ipp.pt>
2024-04-17 22:18:10 +0100

    feat(reactor-svelte): implement multi reactor provider

--- packages/reactor-svelte/src/lib/index.ts
+++ packages/reactor-svelte/src/lib/index.ts
@@ -3,3 +3,4 @@ export * from './typed.js';
 export * from './provider.js';
 export { default as ReactorListener } from './reactor.svelte';
 export { default as ReactorProvider } from './provider.svelte';
+export { default as MultiReactorProvider } from './multi-provider.svelte';
---
+++ packages/reactor-svelte/src/lib/multi-provider.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { provide } from './provider.js';
+	import { type Reactor } from './reactor.js';
+
+	/**
+	 * The reactors that are being provided to children components.
+	 */
+	export let reactors: Reactor<never, never>[];
+
+	reactors.forEach(provide);
+</script>
+
+<slot />
